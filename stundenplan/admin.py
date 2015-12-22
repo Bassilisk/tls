b8448cb518350837b7b6d58b67154f4a94dbf295
Bassilisk <Niklas.Rautschka@t-online.de>
2015-12-22 15:29:17 +0100

    Update admin.py
    Noch nicht kommentiert

--- stundenplan/admin.py
+++ stundenplan/admin.py
@@ -1,21 +1,21 @@
-from django.contrib import admin
+from django.contrib import admin                                #
 
 
 # Register your models here.
-from models import *
+from models import *                                            #                
 
-class PersonS(admin.ModelAdmin):
-    list_display = ("id","vorname","name","jahrgang","klasse")
-class PersonL(admin.ModelAdmin):
-    list_display = ("id","vorname","name","facher")
-    list_display_linls = ("facher",)
+class PersonS(admin.ModelAdmin):                                #                        
+    list_display = ("id","vorname","name","jahrgang","klasse")  #                             
+class PersonL(admin.ModelAdmin):                                #                            
+    list_display = ("id","vorname","name","facher")             #                                
+    #list_display_linls = ("facher",)                           #                                         
     
     
-admin.site.register(Subject)
-admin.site.register(Lehrer,PersonL)
-admin.site.register(Raum)
-admin.site.register(Unterrichtsplan)
-admin.site.register(Schueler,PersonS)
+admin.site.register(Subject)                                    #                           
+admin.site.register(Lehrer,PersonL)                             #                                    
+admin.site.register(Raum)                                       #                                     
+admin.site.register(Unterrichtsplan)                            #                                                            
+admin.site.register(Schueler,PersonS)                           #                                            
 #admin.site.register(Stundenplan)
 
 
