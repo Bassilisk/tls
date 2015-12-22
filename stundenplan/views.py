a89e03dd29428c8bb93ff0b8ab26136531b445b9
Bassilisk <Niklas.Rautschka@t-online.de>
2015-12-22 15:33:37 +0100

    Update views.py
    Erste Kommentierung der Klasse views

--- stundenplan/views.py
+++ stundenplan/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render          #Import der Funktion "render", wird benötigt um ein html layout mit variablen zu referenzieren
+from django.http import HttpResponse         #Import der Klasse "HttpResponse",wird benötigt um eine Http anfrage zu beantworten      
 # Create your views here.
 
-def get_time_table(request):
-    render(request,"static/layout.html",{})
+def get_time_table(request):                #Funktion "get_time_table" wird mit der Variable "request" (http Header und Body) aufgerufen               
+    render(request,"static/layout.html",{}) #gibt die Datei Layout html mit zugewiesenen Variablen zurück    
     
