76974e0e77d83ddf2f0e0ace025d34312b6f33fa
Bassilisk <Niklas.Rautschka@t-online.de>
2015-12-22 14:36:09 +0100

    Update admin.py
    Erste kommentierung zum Programm "admin"

--- vertretungsplan/admin.py
+++ vertretungsplan/admin.py
@@ -1,16 +1,16 @@
-from django.contrib import admin
+from django.contrib import admin                                                #Importieren der Modul Klasse admin, benötigt für Modell Admin
 
-from models import Plan
+from models import Plan                                                         #Importieren des selbst erstellten Modells "Plan"
 
-# Register your models here.
+# Register your models here.                                                            
 
 
-class PlanAdmin(admin.ModelAdmin):
-    day = 'pub_date'
-    list_display = ("day","Unterricht","std","erzsetzt_durch","bemerkung")
-    actions  = "g"
-    def g(self):
-        return "f"
-    #date_hierarchy = "pub_date"
+class PlanAdmin(admin.ModelAdmin):                                              #Erstellung des Administrations Schnittstelle "PlanAdmin" mit Vererbung von "ModelAdmin"
+    day = 'pub_date'                                                            #Sortierung nach Variable "pub_date", sortierung nach Datum aufsteigend 
+    list_display = ("day","Unterricht","std","erzsetzt_durch","bemerkung")      #Projektion der Datenbank Variablen siehe zuordnung
+    actions  = "g"                                                              #test einer funktion (unwichtig)                                                              
+    def g(self):                                                                #test der  Funktion "g"
+    return "f"                                                                  
+    #date_hierarchy = "pub_date"                                                                
 
-admin.site.register(Plan,PlanAdmin)
+admin.site.register(Plan,PlanAdmin)                                             #Registrieung der Schnittstelle "PlanAdmin" an das Framework                    
